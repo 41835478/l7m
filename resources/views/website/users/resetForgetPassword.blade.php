0cce957065ecacd75ba881fab44085f67d7a45c0
mohmmedwee <eloustah@gmail.com>
2017-12-06 12:44:49 +0200

    reset forget password -> done

--- resources/views/website/users/resetForgetPassword.blade.php
+++ resources/views/website/users/resetForgetPassword.blade.php
@@ -3,39 +3,63 @@
 @section('title', Lang::get('dashboard.PageTitle'))
 @section('content')
 
-    @include('website.partials.innerSearch',['title'=> trans('website.forget').' <br><span>'.trans('website.password').'</span>','img'=>'/assets/website/images/contact-ico.png','icon'=>'fa fa-list'])
+    @include('website.partials.innerSearch',['title'=> trans('website.changepassword'),'img'=>'/assets/website/images/add_new_shop.png','icon'=>'fa fa-list'])
+<!--,'title2'=>trans('website.signForm')-->
 
 
-
-    <section class="inner_content_area">
-        <div class="container">
+   <section class="inner-wrapper">
+    <article class="container">
+    
+    <div class="content_wrapper">
+        <!-- <div class="captions_sec">
+            <div class="icon_in"><img src="images/add_new_shop.png" alt=""></div>
+            
+            <h6>Lorem Ipsum is simply dummy text of the printing</h6>
+        </div> -->
+        
+        <section class="sub-wrapper">
             <div class="row">
-                <div class="col-sm-12">
+            <div class="col-md-6 col-sm-10 col-md-offset-3 col-sm-offset-1">
+                <div class="log-wrap">
+                   {!! Form::open([]) !!}
+                    @include('common.partials.messages')
 
-                    <div class="forgot-password col-sm-6 col-sm-offset-3">
-                        {!! Form::open([]) !!}
-                            <div class="form-group">
+                        <!-- <input type="text" placeholder="Current Password" class="text_box5"/> -->
 
-                                @include('common.partials.messages')
 
-                                <label>{{trans('website.enterNewPassword')}}</label>
-                                <input type="password" class="form-control" id="uLogin" placeholder="{{trans('website.password')}}" name="password">
-                                <input type="password" class="form-control" id="uLogin" placeholder="{{trans('website.confirmPassword')}}" name="confirmPassword">
-                                <hr>
-                                <button type="submit" class="btn btn-primary">{{trans('website.submit')}}</button>
-                            </div> <!-- /.form-group -->
+                        <input type="password" class="text_box5" id="uLogin" placeholder="{{trans('website.password')}}" name="password">
 
-                      {!! Form::close()  !!}
 
-                    </div> <!-- /.modal-body -->
+                        <input type="password" class="text_box5" id="uLogin" placeholder="{{trans('website.confirmPassword')}}" name="confirmPassword">
 
+                        
+                        <button type="submit" class="submit_btn" >">{{trans('website.updatePass')}}
+                        </button>
+                        
+                        
+                        
+                        
+                          {!! Form::close()  !!}
+                        
+                    
+                    
+                    
                 </div>
-
+                
+                
             </div>
+            
+            
+            
         </div>
+        </section>
+        
+        
+    </div>
 
 
-    </section>
+    </article>
+</section>
 
 
 @stop
---
+++ resources/views/website/users/signIn.blade.php
@@ -0,0 +1,65 @@
+@extends('website.layouts.inner')
+@section('title', Lang::get('website.createAccount'))
+@section('content')
+
+@include('website.partials.innerSearch',['title'=>trans('website.signIn'),'img'=>'/assets/website/images/sign-icon.png','icon'=>'fa fa-list'])
+<!--,'title2'=>trans('website.signForm')-->
+
+
+<section class="inner-wrapper">
+  <article class="container">
+  
+  <div class="content_wrapper">
+    <!-- <div class="captions_sec">
+      <div class="icon_in"><img src="/assets/website/images/sign-icon.png" alt=""></div>
+      
+      <h6>Lorem Ipsum is simply dummy text of the printing</h6>
+    </div> -->
+    
+    <section class="sub-wrapper">
+      <div class="row">
+      <div class="col-md-6 col-sm-10 col-md-offset-3 col-sm-offset-1">
+        <div class="log-wrap">
+          {!! Form::open(['id'=>'loginform' ]) !!}
+           <!--  <input type="text" placeholder="Username" class="text_box5"/> -->
+            {!! Form::text('email', null, ['class'=>'text_box5','placeholder'=>trans('user.email')]) !!}
+          <!--   <input type="password" placeholder="Password" class="text_box5"/> -->
+          {!! Form::password('password', ['class'=>'text_box5','placeholder'=>trans('user.password')]) !!}
+            <div class="sub-line">
+              <input type="checkbox"><label>Remember Me</label>
+              <a href="/recover">Forgot Password</a>
+            </div>
+            <button type="submit" class="submit_btn" >{{trans('user.signIn')}}</button>
+            
+            <ul class="log-way">
+              <li><a href="#"><img src="/assets/website/images/facebook.png" alt="" title="facebook"></a></li>
+              <li><a href="#"><img src="/assets/website/images/twitter.png" alt="" title="twitter"></a></li>
+              <li><a href="#"><img src="/assets/website/images/google.png" alt="" title="google +"></a></li>
+            </ul>
+            
+            <div class="reg_link">Don't have an account ? <a href="/user/create">Register Now</a></div>
+            
+            
+            
+       
+          
+          
+        </div>
+        
+        
+      </div>
+      
+      
+      
+    </div>
+    </section>
+    
+    
+  </div>
+
+
+  </article>
+</section>
+
+   
+    @stop
